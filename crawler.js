216dfcf23d13f5c9e5df7e832ac8c85729df5bda
Trọng Hiếu <tronghieu60s@gmail.com>
2020-09-25 08:07:12 +0700

    update api

--- crawler.js
+++ crawler.js
@@ -1,56 +1,63 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var Crawler = require("crawler");
-let covid19Model = require('./models/covid19');
+let covid19Model = require("./models/covid19");
 
 module.exports = function crawler() {
-    vietnam();
-    world();
-}
+  vietnam();
+  world();
+};
 
-function vietnam() {
-    var vietnam = new Crawler({
-        callback: function (error, res, done) {
-            if (error) {
-                console.log(error);
-            } else {
-                var $ = res.$;
-                let cases = $(".box-tke .fivecolumns:nth-child(1) div:nth-child(2) span").text();
-                let deaths = $(".box-tke .fivecolumns:nth-child(1) div:nth-child(5) span").text();
-                let recovered = $(".box-tke .fivecolumns:nth-child(1) div:nth-child(4) span").text();
-                let vietnam = { cases, deaths, recovered };
-                if (cases !== "") {
-                    covid19Model.findOneAndUpdate({ success: true }, { vietnam }, (err) => {
-                        if (err) console.log(err);
-                    })
-                }
+const vietnam = () => {
+  const vietnam = new Crawler({
+    callback: function (error, res, done) {
+      if (error) {
+        console.log(error);
+      } else {
+        const $ = res.$;
+        const cases = $(".font24").eq(0).text();
+        const treatment = $(".font24").eq(1).text();
+        const recovered = $(".font24").eq(2).text();
+        const deaths = $(".font24").eq(3).text();
+        const vietnam = { cases, treatment, deaths, recovered };
+        if (cases !== "")
+          covid19Model.findOneAndUpdate(
+            { success: true },
+            { vietnam },
+            (err) => {
+              if (err) console.log(err);
             }
-            done();
-        }
-    });
-    vietnam.queue('https://ncov.moh.gov.vn/');
-}
+          );
+      }
+      done();
+    },
+  });
+  vietnam.queue("https://ncov.moh.gov.vn/");
+};
 
-function world() {
-    var world = new Crawler({
-        callback: function (error, res, done) {
-            if (error) {
-                console.log(error);
-            } else {
-                var $ = res.$;
-                let data = $("div[class='maincounter-number'] > span");
-                let cases = $(data[0]).text().trim();
-                let deaths = $(data[1]).text().trim();
-                let recovered = $(data[2]).text().trim();
-                let global = { cases, deaths, recovered };
+const world = () => {
+  const world = new Crawler({
+    callback: function (error, res, done) {
+      if (error) {
+        console.log(error);
+      } else {
+        const $ = res.$;
+        const data = $("div[class='maincounter-number'] > span");
+        const cases = $(data[0]).text().trim();
+        const deaths = $(data[1]).text().trim();
+        const recovered = $(data[2]).text().trim();
+        const global = { cases, deaths, recovered };
 
-                if (cases !== "") {
-                    covid19Model.findOneAndUpdate({ success: true }, { global }, (err) => {
-                        if (err) console.log(err);
-                    })
-                }
+        if (cases !== "")
+          covid19Model.findOneAndUpdate(
+            { success: true },
+            { global },
+            (err) => {
+              if (err) console.log(err);
             }
-            done();
-        }
-    });
-    world.queue('https://www.worldometers.info/coronavirus/');
-}
+          );
+      }
+      done();
+    },
+  });
+  world.queue("https://www.worldometers.info/coronavirus/");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-let covid19Model = require('../models/covid19');
-const crawler = require('../crawler');
+let covid19Model = require("../models/covid19");
+const crawler = require("../crawler");
+
 crawler();
 setInterval(() => {
   crawler();
-}, 300000)
+}, 300000);
 
-router.get('/', function (req, res, next) {
-  res.redirect('/api');
+router.get("/", function (req, res, next) {
+  res.redirect("/api");
 });
 
-router.get('/api', function (req, res, next) {
+router.get("/api", function (req, res, next) {
   covid19Model.findOne({ success: true }, function (err, covid19) {
     if (covid19) {
       let { vietnam, global, countries } = covid19;
       let data = { vietnam, global, countries };
       res.send({ success: true, data });
     }
-  })
+  });
 });
 
 module.exports = router;
